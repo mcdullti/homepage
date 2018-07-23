cea0c0863bdd098f6605ec98b5fd8d1fba3245da
Aaron <trollolol1029@gmail.com>
2018-07-23 23:08:05 +0800

    test

--- docs/scroll.js
+++ docs/scroll.js
@@ -24,22 +24,4 @@ $(document).ready(function(){
     ;
   }
   $original.remove();
-
-  var last=Date.now();
-  var y=0;
-  var speed=0.015;
-  (function updateScroll(){
-    var now=Date.now();
-    var deltaT=now-last;
-    y+=speed*deltaT;
-    y=y % ($(".layer").height()/2);
-    TweenMax.set($(".layer"),{
-      y:-y,
-      force3D:true,
-      transformOrigin:"50% "+(($(window).height()/3)+y)+"px"
-    });
-    last=now;
-
-    //requestAnimationFrame(updateScroll);
-  }());
 })
