112c8e139a8f3dd352f156c5491779058d40b5bc
Aaron <trollolol1029@gmail.com>
2018-07-23 22:31:52 +0800

    test

--- docs/scroll2.js
+++ docs/scroll2.js
@@ -1,19 +1,9 @@
 $("#plotly-button").click(function(){
-  var last=Date.now();
-  var y;
-  var speed=0.015;
-  $(document).toggle(function updateScroll(){
-    var now=Date.now();
-    var deltaT=now-last;
-    y+=speed*deltaT;
-    y=y % ($(".layer").height()/2);
-    TweenMax.set($(".layer"),{
-      y:-y,
-      force3D:true,
-      transformOrigin:"50% "+(($(window).height()/3)+y)+"px"
-    });
+  var speed=0;
+  $(document).toggle(
+    speed=0.015;
+  )
     last=now;
-
     requestAnimationFrame(updateScroll);
   }());
   })
