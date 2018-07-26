7406b5e1746a906ef83c16e9edd6069bae0ddf52
mcdullti <trollolol1029@gmail.com>
2018-07-26 13:07:42 +0800

    test

--- docs/background.js
+++ docs/background.js
@@ -2,7 +2,6 @@ $( document ).ready(function() {
     var isMobile = window.matchMedia("only screen and (max-width: 760px)");
 
     if (isMobile.matches) {
-      $("p").hide();
       $("#con").hide();
     }
  });
