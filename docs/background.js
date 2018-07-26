642e44099a132a10821a764b1d3a0e9bce32b359
mcdullti <trollolol1029@gmail.com>
2018-07-26 13:12:20 +0800

    fix

--- docs/background.js
+++ docs/background.js
@@ -2,6 +2,7 @@ $( document ).ready(function() {
     var isMobile = window.matchMedia("only screen and (max-width: 760px)");
 
     if (isMobile.matches) {
+      $("p").hide();
       $("#con").hide();
     }
  });
