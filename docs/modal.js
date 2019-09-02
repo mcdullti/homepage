4525b88cec3c804e78289a902f76e35b0610ff5f
mcdullti <40600110+mcdullti@users.noreply.github.com>
2019-09-02 21:30:13 +0800

    Create modal.js

---
+++ docs/modal.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    var isMobile = window.matchMedia("only screen and (max-width: 760px)");
+
+    if (isMobile.matches) {
+      $("p").hide();
+      $("#con").hide();
+      $("#cone").hide();
+      $("#conn").hide();
+      $("#modal1").hide();
+    }
+ });
